f0cc6f840e66dcc6608e94e4c1a358eabfa52ed4
sijahmn <msijahmn@gmail.com>
2022-06-17 01:39:06 +0530

    bmi calculator comoleted

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-weight = document.getElementById("weight");
-height = document.getElementById("height");
-btn = document.getElementById("btn-submit");
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
+const btn = document.getElementById("btn-submit");
 
-result = document.getElementById("result");
-resultStatement = document.getElementById("result-statement");
+const result = document.getElementById("result");
+const resultStatement = document.getElementById("result-statement");
 
 
-weight.value
+function bmiFun() {
+    const weightValue = weight.value
+    const HeightValue = height.value / 100
+
+    let bmi = weightValue / (HeightValue * HeightValue)
+    bmi = Math.round(bmi)
+    return bmi
+
+
+}
+
+btn.addEventListener("click", function () {
+    const bmiReturn = bmiFun()
+    result.innerHTML = bmiReturn;
+    let statement;
+    if (bmiReturn > 24.9) {
+        statement = "OVER HEIGHT"
+    }
+    else if (bmiReturn < 24.9 && bmiReturn > 18.5) {
+        statement = "HEALTHY"
+
+    } else {
+        statement = "UNDER HEIGHT"
+    }
+
+    resultStatement.innerHTML = `Your BMI STATUS is ${statement}`;
+
+})
+
