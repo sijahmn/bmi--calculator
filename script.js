3890795f81a01e5b9f7554ab478be89e599c2ce0
sijahmn <msijahmn@gmail.com>
2022-06-16 23:38:35 +0530

    initally all set

---
+++ script.js
@@ -0,0 +1,9 @@
+weight = document.getElementById("weight");
+height = document.getElementById("height");
+btn = document.getElementById("btn-submit");
+
+result = document.getElementById("result");
+resultStatement = document.getElementById("result-statement");
+
+
+weight.value
